d855f4ab41c47a5e318c3512d4b580be077553ba
Joe Brown <brownj8@tcd.ie>
2023-03-08 11:52:14 +0000

    Moved string sanitisation to separate function and added myNumber

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import qualified Data.Aeson.KeyMap as AKM
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.List as L
 import qualified Data.Map.Strict as MS
+import qualified Data.Scientific as S
 import qualified Data.Vector as V
 
 main :: IO ()
@@ -34,23 +35,29 @@ data MyValue
   = MyObject [(String, MyValue)]
   | MyArray [MyValue]
   | MyString String
---   | MyNumber Scientific
+  | MyNumber Float 
   | MyBool Bool
   | MyNull
   deriving (Show)
 
 -- Converts Value Data Type to custom MyValue Data Type
 toMyValue :: Value -> MyValue
-toMyValue (Object obj) = MyObject $ L.map (\(k, v) -> (show k, toMyValue v)) $ MS.toList $ AKM.toMap obj
+toMyValue (Object obj) = MyObject $ L.map (\(k, v) -> (sanitiseString $ show k, toMyValue v)) $ MS.toList $ AKM.toMap obj
 toMyValue (Array arr) = MyArray $ L.map (\k -> toMyValue k) $ V.toList arr
 toMyValue (String str) = MyString $ unpack str
--- toMyValue (Number num) = MyNumber num
+toMyValue (Number num) = MyNumber (S.toRealFloat num)
 toMyValue (Bool bool) = MyBool bool
 toMyValue Null = MyNull
 
+-- checks the item type and prints message based on that
 checkType :: MyValue -> IO ()
-checkType (MyObject obj) = case lookup "type" (L.map (\(k, v) -> (Prelude.drop 1 . Prelude.init $ k, v)) obj) of
+checkType (MyObject obj) = case lookup "type" obj of
     Just (MyString "requirement") -> putStrLn "Item Type is Requirement"
     Just x -> putStrLn "unrecognised item type"
     Nothing -> putStrLn "Error"
 checkType _ = putStrLn "Error: not an object"
+
+-- removes the two quotation marks from either side of a string that has
+-- been processed with the "show" function
+sanitiseString :: String -> String
+sanitiseString = Prelude.drop 1 . Prelude.init
